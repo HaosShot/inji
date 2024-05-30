5b5c9ca95000e46f8d0acea8212fd8077248006b
Admin <Admin@DESKTOP-QFTHS3A>
2024-05-30 23:43:43 +0300

    Добавьте файлы проекта.

---
+++ inji/inji.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+void drawTriangle(int n) 
+{
+   
+    for (int i = 1; i <= n; ++i) 
+    {
+        
+        for (int j = 1; j <= i; ++j)
+        {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    int height;
+
+    std::cout << "Введите высоту треугольника: ";
+    std::cin >> height;
+
+    if (height <= 0) 
+    {
+        std::cout << "Высота треугольника должна быть положительным числом." << std::endl;
+        return 1;
+    }
+
+    drawTriangle(height);
+
+    return 0;
+}
